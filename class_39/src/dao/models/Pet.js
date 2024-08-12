b00ae672515b53d836681f1962b5cb6cd460f492
Carlos Perren <idux.net@gmail.com>
2024-08-12 09:17:29 -0300

    Proyecto Adoptme base p/ docs

---
+++ class_39/src/dao/models/Pet.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+mongoose.pluralize(null);
+
+const collection = 'adoptme_pets';
+
+const schema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    specie:{
+        type:String,
+        required:true
+    },
+    birthDate:Date,
+    adopted:{
+        type:Boolean,
+        default:false
+    },
+    owner:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:'Users'
+    },
+    image:String
+})
+
+const petModel = mongoose.model(collection,schema);
+
+export default petModel;
